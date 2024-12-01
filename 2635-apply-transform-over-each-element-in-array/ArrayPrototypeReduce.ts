5dcfe2c03336b4a7176d9a6ef0005795cd80534e
victor langat <provicml@gmail.com>
2024-12-01 08:13:41 +0300

    Files added: Array reduce method

---
+++ 2635-apply-transform-over-each-element-in-array/ArrayPrototypeReduce.ts
@@ -0,0 +1,13 @@
+// use  Array.prototype.reduce
+
+
+type Fn = (n: number, i: number) => any
+
+function filter(arr: number[], fn: Fn): number[] {
+    return arr.reduce((result: number[], val: number, index: number) => {
+        if (fn(val, index)) {
+            result.push(val)
+        }
+        return result
+    },[])
+};
